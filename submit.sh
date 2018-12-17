72e2f9c3fd60e35f62f76b69ef4e4e51b727aa53
Richard Neher <richard.neher@unibas.ch>
2018-12-17 12:00:13 +0100

    add submit scripts

---
+++ submit.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Reserve 1 CPUs for this job
+#
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8G
+#
+# Request it to run this for HH:MM:SS with ?G per core
+#
+#SBATCH --time=05:59:00
+
+source ~/.bashrc
+ml FastTree
+
+~/miniconda3/bin/python $@
