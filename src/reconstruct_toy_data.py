6a9a0ce3cfecfb917a673d1deb81bb092a96be05
Richard Neher <richard.neher@unibas.ch>
2019-04-28 14:56:52 +0200

    reorganize reconstruction script

--- src/reconstruct_toy_data.py
+++ src/reconstruct_toy_data.py
@@ -17,6 +17,14 @@ def KL(p,q):
 def chisq(p,q):
     return np.sum((p-q)**2)
 
+def assess_reconstruction(model, true_model):
+    return {"chisq_p": np.mean([chisq(model.Pi[:,i],true_model.Pi[:,i]) for i in range(params['L'])]),
+            "model_entropy": -np.mean(np.sum(model.Pi*np.log(model.Pi), axis=0)),
+            "true_entropy": -np.mean(np.sum(true_model.Pi*np.log(true_model.Pi), axis=0)),
+            "chisq_mu": chisq(model.mu/model.average_rate().mean(), true_model.mu/true_model_average_rate),
+            "chisq_W":chisq(model.W.flatten(), true_model.W.flatten())}
+
+
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description = "", usage="analyze simulated data for site specific GTR reconstruction project")
@@ -25,6 +33,7 @@ if __name__ == '__main__':
     parser.add_argument("-L", type=int, help="length of sequence")
     parser.add_argument("--prefix", type=str, help="folder to save data")
     parser.add_argument("--aa", action='store_true', help="use amino acid alphabet")
+    parser.add_argument("--pc", type=float, default=1.0, help="pseudocount for reconstruction")
     args=parser.parse_args()
 
     prefix = args.prefix
@@ -41,79 +50,95 @@ if __name__ == '__main__':
     mu_vals = set()
     n_vals = set()
 
-    pc=0.1
-    niter=5
-
-    analysis_types = ['naive', 'single', 'dressed', 'regular', 'regular_true',
-                      'marginal','marginal_true', 'iterative', 'iterative_true']
+    pc = args.pc
+    analysis_types_tree = [
+        'single', # estimate a single model for all sites
+        'regular', # reconstruct counts using JC model
+        'marginal', # use expected number of counts
+        'iterative', # iterate model estimation and reconstruction
+        'optimize_tree', #estimate model and branch length interatively
+    ]
 
     alphabet='aa_nogap' if args.aa else 'nuc_nogap'
+
     for fname in files:
         print(fname)
 
         params = parse_alignment_name(fname)
+        dset = (params['L'], params['n'], params['m'])
         mu_vals.add(params['m'])
         n_vals.add(params['n'])
 
+        # load true model, and aln
         true_model = load_model(model_name(prefix, params))
         true_model_average_rate = true_model.average_rate().mean()
         true_mut_counts = load_mutation_count(mutation_count_name(prefix, params))
-        dset = (params['L'], params['n'], params['m'])
 
-        _mc, true_LH, true_tree = reconstruct_counts(prefix, params, gtr=true_model, alphabet=alphabet,
-                                              marginal=True, reconstructed_tree=False)
-        true_tree_length = true_tree.tree.total_branch_length()
-        for ana in analysis_types:
-            if ana=='naive':
-                naive = p_from_aln(prefix, params, alphabet=alphabet)
-                p_dist[ana][dset].append(np.mean([chisq(naive[:,i],true_model.Pi[:,i]) for i in range(params['L'])]))
-                p_entropy[ana][dset].append([-np.mean(np.sum(naive*np.log(naive+1e-10), axis=0)),
-                                            -np.mean(np.sum(true_model.Pi*np.log(true_model.Pi), axis=0))])
-            else:
-                if ana=='dressed':
-                    mc = (true_mut_counts, None, None)
-                    bl = [n.branch_length for n in _t.tree.find_clades() if n!=_t]
-                elif ana in ['iterative', 'iterative_true']:
-                    model = 'JC69'
-                    for i in range(niter):
-                        mc = reconstruct_counts(prefix, params, gtr=model,
-                                                alphabet=alphabet, marginal=True,
-                                                reconstructed_tree=ana=='iterative')
-                        model = estimate_GTR(mc[0], pc=pc, single_site=False, alphabet=alphabet)
-                        print(i, np.mean([chisq(model.Pi[:,i],true_model.Pi[:,i]) for i in range(params['L'])]))
-                elif ana=='branch_length':
-                    model = 'JC69'
-                    kappa=1.0
-                    print(true_model.mu[:5])
-                    for i in range(niter):
-                        mc = reconstruct_counts(prefix, params, gtr=model,
-                                                alphabet=alphabet, marginal=True,
-                                                reconstructed_tree=False)
-                        model = estimate_GTR(mc[0], pc=pc, single_site=False, tt=mc[-1] if i else None, alphabet=alphabet)
-                        mc[-1].set_gtr(model)
-                else:
-                    rec_model = {'true_model':true_model}
-                    mc = reconstruct_counts(prefix, params, gtr=rec_model.get(ana, 'JC69'),
-                                            alphabet=alphabet, marginal=ana in ['marginal', 'true_model', 'iterative'],
-                                            reconstructed_tree=('true' not in ana))
+        with gzip.open(alignment_name(prefix, params), 'rt') as fh:
+            aln = AlignIO.read(fh, 'fasta')
 
-                if ana not in ['iterative', 'branch_length']:
-                    model = estimate_GTR(mc[0], pc=pc, single_site=ana=='single', alphabet=alphabet)
+        tt_true = TreeAnc(tree=tree_name(prefix, params), aln=aln, gtr=true_model, reduce_alignment=False)
+        tt_true.infer_ancestral_sequences(marginal=True)
+        true_tree_length = tt_true.tree.total_branch_length()
+
+        ## tree less methods
+        # alignment frequencies
+        naive = p_from_aln(aln, alphabet=alphabet)
+        p_dist["naive"][dset].append(np.mean([chisq(naive[:,i],true_model.Pi[:,i]) for i in range(params['L'])]))
+        p_entropy["naive"][dset].append([-np.mean(np.sum(naive*np.log(naive+1e-10), axis=0)),
+                                     -np.mean(np.sum(true_model.Pi*np.log(true_model.Pi), axis=0))])
+
+        # use true counts, no tree
+        model = estimate_GTR(true_mut_counts, pc=pc, single_site=False, alphabet=alphabet)
+        accuracy = assess_reconstruction(true_model, model)
+        p_dist["dressed"][dset].append(accuracy["chisq_p"])
+        p_entropy["dressed"][dset].append([accuracy["model_entropy"], accuracy["true_entropy"]])
+        mu_dist["dressed"][dset].append(accuracy["chisq_mu"])
+        W_dist["dressed"][dset].append(accuracy["chisq_W"])
+
+
+        for tree in [tree_name(prefix, params), reconstructed_tree_name(prefix, params)]:
+            for ana in analysis_types_tree:
+                T = Phylo.read(tree, format="newick")
+                if tree == tree_name(prefix, params):
+                    for n in T.find_clades():
+                        n.branch_length *= params['m']
+
+                tt = TreeAnc(tree=T, aln = aln, reduce_alignment=False, alphabet=alphabet)
+
+                if ana=="iterative":
+                    # this performs iterative estimation of the model and ancestral sequences
+                    # using marginal ancestral reconstruction
+                    tt.infer_gtr_iterative(normalized_rate=False, site_specific=True, pc=pc)
+                elif ana=="single":
+                    tt.infer_gtr(marginal=False, normalized_rate=False, site_specific=False, pc=pc)
+                elif ana=="regular":
+                    tt.infer_gtr(marginal=False, normalized_rate=False, site_specific=True, pc=pc)
+                elif ana=="marginal":
+                    tt.infer_gtr(marginal=True, normalized_rate=False, site_specific=True, pc=pc)
+                elif ana=='optimize_tree':
+                    tt.optimize_tree(branch_length_mode='marginal', max_iter=10,
+                                     infer_gtr=True, site_specific_gtr=True, pc=pc)
+
+                # calculate likelihood
+                tt.infer_ancestral_sequences(marginal=True)
+
+                model = tt.gtr
+                np.fill_diagonal(model.W,0)
                 avg_rate[ana][dset].append((true_model_average_rate, model.average_rate().mean()))
-                _mc, model_LH, _t = reconstruct_counts(prefix, params, gtr=model, alphabet=alphabet,
-                                                       marginal=True, reconstructed_tree=True)
-                delta_LH[ana][dset].append( (true_LH, model_LH) )
+                delta_LH[ana][dset].append( (tt_true.sequence_LH(), tt.sequence_LH() ))
 
                 if ana!='single':
-                    p_dist[ana][dset].append(np.mean([chisq(model.Pi[:,i],true_model.Pi[:,i]) for i in range(params['L'])]))
-                    p_entropy[ana][dset].append([-np.mean(np.sum(model.Pi*np.log(model.Pi), axis=0)),
-                                                -np.mean(np.sum(true_model.Pi*np.log(true_model.Pi), axis=0))])
-                    mu_dist[ana][dset].append(chisq(model.mu/model.average_rate().mean(), true_model.mu/true_model_average_rate))
+                    accuracy = assess_reconstruction(true_model, model)
+                    p_dist[ana][dset].append(accuracy["chisq_p"])
+                    p_entropy[ana][dset].append([accuracy["model_entropy"], accuracy["true_entropy"]])
+                    mu_dist[ana][dset].append(accuracy["chisq_mu"])
+                    W_dist[ana][dset].append(accuracy["chisq_W"])
+                else:
+                    W_dist[ana][dset].append(chisq(model.W.flatten(), true_model.W.flatten()))
 
-                np.fill_diagonal(model.W,0)
-                W_dist[ana][dset].append(chisq(model.W.flatten(), true_model.W.flatten()))
 
-    out_prefix = prefix + '_results_pc_%1.1f/'%pc
+    out_prefix = prefix + '_results_pc_%1.2f/'%pc
     if not os.path.isdir(out_prefix):
         os.mkdir(out_prefix)
 
